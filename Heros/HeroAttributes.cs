913e4d3f24aca25eecbb4539681b51dff72c2e10
Danielle <ninoue.hamrin@gmail.com>
2023-02-07 16:59:11 +0100

    feat: started constructing baseHero class

---
+++ Heros/HeroAttributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_heros_c.Heros
+{
+    public class HeroAttributes
+    {
+        public int Strength { get; set; }
+
+        public int Dexterity { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public HeroAttributes(int strength, int dexterity, int intelligence)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+    }
+}
---
+++ Heros/HeroBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_heros_c.Heros
+{
+    abstract class HeroBase
+    {
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public HeroAttributes Attributes { get; set; }
+
+        public abstract WeaponTypes[];
+
+
+    }
+}
